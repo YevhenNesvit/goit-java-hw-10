de5bafe43d106adbc51ef6993f582e89736e9368
Yevhen Nesvit <86827674+YevhenNesvit@users.noreply.github.com>
2022-06-16 12:25:53 +0300

    initial commit

--- PhoneNumbers.java
+++ PhoneNumbers.java
@@ -1,10 +1,10 @@
 import java.io.*;
 
 public class PhoneNumbers {
-    private static  final String FILE_PATH = "C:\\Users\\Yevhen\\IdeaProjects\\goit-java-hw-10\\src\\main\\resources\\file1.txt";
+    private static  final File FILE_PATH = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "file1.txt");
 
     public static void validPhoneNumbers() {
-        try(FileInputStream fileInputStream = new FileInputStream(FILE_PATH)) {
+        try(FileInputStream fileInputStream = new FileInputStream(FILE_PATH.getAbsolutePath())) {
             byte[] buffer = new byte[fileInputStream.available()];
             fileInputStream.read(buffer);
             String strings = "";
@@ -13,7 +13,7 @@ public class PhoneNumbers {
                 strings += (char) b;
             }
 
-            String[] list = strings.split("\r\n");
+            String[] list = strings.split(System.lineSeparator());
             for (int i = 0; i < list.length; i++) {
                 if (!(list[i].matches("^\\d{3}-\\d{3}-\\d{4}") || list[i].matches("^\\(\\d{3}\\)\\s\\d{3}-\\d{4}"))) {
                     list[i] = null;
