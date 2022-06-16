12d02d5d57e9f979522249660495b1e910aac3f9
Yevhen Nesvit <86827674+YevhenNesvit@users.noreply.github.com>
2022-06-16 12:27:10 +0300

    initial commit

--- TextToJson.java
+++ TextToJson.java
@@ -4,19 +4,19 @@ import java.io.*;
 import java.util.*;
 
 public class TextToJson {
-    private static final String INPUT_FILE_PATH = "C:\\Users\\Yevhen\\IdeaProjects\\goit-java-hw-10\\src\\main\\resources\\file2.txt";
-    private static final String OUTPUT_FILE_PATH = "C:\\Users\\Yevhen\\IdeaProjects\\goit-java-hw-10\\src\\main\\resources\\user.json";
+    private static final File INPUT_FILE_PATH = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "file2.txt");
+    private static final File OUTPUT_FILE_PATH = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "user.json");
 
     public static void convert() {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
-             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE_PATH.getAbsolutePath()));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH.getAbsolutePath()))) {
             int c;
             String strings = "";
 
             while ((c = bufferedReader.read()) != -1) {
                 strings += (char) c;
             }
-            String[] l = strings.split("\r\n");
+            String[] l = strings.split(System.lineSeparator());
             String li = "";
 
             for (int i = 1; i < l.length; i++) {
@@ -34,7 +34,7 @@ public class TextToJson {
             for (int i = 0; i < userList.size(); i++) {
                 user[i] = gson.toJson(userList.get(i));
             }
-
+//            System.out.println(Arrays.toString(user));
             bufferedWriter.write(Arrays.toString(user));
             bufferedWriter.flush();
         } catch (IOException e) {
